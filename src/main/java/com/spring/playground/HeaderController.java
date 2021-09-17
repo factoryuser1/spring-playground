123b2c4f87c8ee5959fd70d5c906f16e6659c65e
factoryuser1 <factoryuser1@yahoo.com>
2021-09-17 12:01:19 -0700

    Updated as of 1200pm

--- src/main/java/com/spring/playground/HeaderController.java
+++ src/main/java/com/spring/playground/HeaderController.java
@@ -1,8 +1,8 @@
 package com.spring.playground;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class HeaderController {
@@ -10,5 +10,16 @@ public class HeaderController {
     @GetMapping("/header")
     public String getHeaderValue(@RequestHeader String host){
         return host;
+    }
+
+    @PostMapping("/posts/{postId}/comments")
+    public String createComment(@PathVariable("postId") int postId, @RequestParam Map<String , String> params){
+        return String.format(
+                "postId:%d notify:%s content:%s author:%s",
+                postId,
+                params.get("notify"),
+                params.get("content"),
+                params.get("authoer")
+        );
     }
 }
